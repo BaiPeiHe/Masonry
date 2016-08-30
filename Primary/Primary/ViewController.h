4081c83c442c8ff98cf894ddc77329dfda33687b
白鹤 <baihe@baihedeMacBook-Air.local>
2016-08-30 16:14:17 +0800

    创建工程 create program

---
+++ Primary/Primary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Primary
+//
+//  Created by 白鹤 on 16/8/30.
+//  Copyright © 2016年 白鹤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Primary/Primary/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Primary
+//
+//  Created by 白鹤 on 16/8/30.
+//  Copyright © 2016年 白鹤. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
