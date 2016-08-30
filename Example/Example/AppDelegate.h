38cfbcfc43c746db51ddbcbe80f7f946f24a8d1d
白鹤 <baihe@baihedeMacBook-Air.local>
2016-08-30 16:28:24 +0800

    创建工程,包含 Masonry 的实例

---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by 白鹤 on 16/8/30.
+//  Copyright © 2016年 白鹤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
