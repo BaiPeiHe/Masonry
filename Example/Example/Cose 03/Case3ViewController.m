72b2d275ef095628bd644ec7316ccc3915cb21da
白鹤 <392275709@qq.com>
2016-08-31 16:35:42 +0800

    完成第三个实例,子视图的尺寸  总是 父视图 尺寸的 1/2

--- Example/Example/Cose 03/Case3ViewController.m
+++ Example/Example/Cose 03/Case3ViewController.m
@@ -13,6 +13,9 @@
 @end
 
 @implementation Case3ViewController
+{
+    UIView *containerView;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -26,12 +29,72 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
-    UIView *containerView = [UIView new];
+    containerView = [UIView new];
     [self.view addSubview:containerView];
     
+    containerView.backgroundColor = [UIColor yellowColor];
     
     
+    UIView *subView = [UIView new];
+    [containerView addSubview:subView];
+    
+    subView.backgroundColor = [UIColor brownColor];
+    
+    
+    UISlider *slider = [UISlider new];
+    [self.view addSubview:slider];
+    
+    [slider addTarget:self action:@selector(updateContainerViewFram:) forControlEvents:UIControlEventValueChanged];
+    
+    
+    [containerView mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.top.equalTo(self.view.mas_top).with.offset(200);
+        make.centerX.equalTo(self.view.mas_centerX);
+        
+        make.height.equalTo(@200);
+        make.width.equalTo(self.view.mas_width).with.offset(-20);
+        
+    }];
+    
+    [subView mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.top.equalTo(containerView.mas_top).with.offset(10);
+        
+        // 居中
+        make.centerX.equalTo(containerView.mas_centerX);
+        
+        // 宽度为 父视图的 1/2
+        make.width.equalTo(containerView.mas_width).multipliedBy(0.5);
+        
+        // 高度为 父视图的 1/3
+        make.height.equalTo(containerView.mas_height).multipliedBy(0.3);
+        
+    }];
+    
+    [slider mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.top.equalTo(containerView.mas_bottom).with.offset(20);
+        make.centerX.equalTo(self.view.mas_centerX);
+        
+        make.width.equalTo(self.view.mas_width).with.offset(-50);
+    }];
 }
+
+- (void)updateContainerViewFram:(UISlider *)sender{
+    
+    [containerView mas_updateConstraints:^(MASConstraintMaker *make) {
+       
+        NSLog(@"%f",sender.value);
+        CGFloat offsetWidth = self.view.frame.size.width * sender.value + 20;
+        make.width.equalTo(self.view.mas_width).with.offset(-offsetWidth);
+        
+        CGFloat offsetHeight = self.view.frame.size.height - 200 + 200 * sender.value;
+        make.height.equalTo(self.view.mas_height).with.offset(-offsetHeight);
+        
+    }];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
